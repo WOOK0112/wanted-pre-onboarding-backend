18c41a150cb3c38cf7309b409704858cb93aa117
wook <dong930112@gmail.com>
2023-10-07 01:55:58 +0900

    Test: "CompanyPost 기능 테스트"

--- src/test/java/com/wanted/assignment/AssignmentApplicationTests.java
+++ src/test/java/com/wanted/assignment/AssignmentApplicationTests.java
@@ -1,13 +1,36 @@
 package com.wanted.assignment;
 
+import com.google.gson.Gson;
+import com.wanted.assignment.company.Company;
+import com.wanted.assignment.company.CompanyRepository;
+import com.wanted.assignment.notice.dto.NoticePostDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AssignmentApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private Gson gson;
+
+	@Autowired
+	private CompanyRepository companyRepository;
 
 	@Test
-	void contextLoads() {
+	void postCompanyTest() {
+		Company company = new Company();
+		company.setName("원티드");
+		company.setNation("한국");
+		company.setRegion("서울");
+
+		companyRepository.save(company);
 	}
+
 
 }
