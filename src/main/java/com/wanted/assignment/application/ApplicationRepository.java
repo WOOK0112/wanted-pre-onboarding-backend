0552416b98f5d5a752e6e525c85a87c78ca0cdc1
wook <dong930112@gmail.com>
2023-10-10 00:57:41 +0900

    Feat: Application API 채용공고 중복확인 기능 구현

--- src/main/java/com/wanted/assignment/application/ApplicationRepository.java
+++ src/main/java/com/wanted/assignment/application/ApplicationRepository.java
@@ -1,6 +1,12 @@
 package com.wanted.assignment.application;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
+
+    Application findByNoticeNoticeIdAndMemberMemberId(Long noticeId, Long memberId);
+
 }
--- src/main/java/com/wanted/assignment/application/ApplicationService.java
+++ src/main/java/com/wanted/assignment/application/ApplicationService.java
@@ -6,6 +6,8 @@ import com.wanted.assignment.notice.service.NoticeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @Slf4j
 public class ApplicationService {
@@ -21,11 +23,23 @@ public class ApplicationService {
     }
 
     public Application createApplication(Long noticeId, Long memberId) {
+
+        //이미 지원한 적 있는 공고인지 검증하는 로직
+        Optional<Application> applicationOptional = Optional.ofNullable(applicationRepository.findByNoticeNoticeIdAndMemberMemberId(noticeId, memberId));
+
+        applicationOptional.ifPresent(application -> {
+            throw new RuntimeException("Application already exists");
+        });
+
         Application application = new Application();
         application.setMember(memberService.getMember(memberId));
         application.setNotice(noticeService.getNotice(noticeId));
 
         return applicationRepository.save(application);
+    }
+
+    public Application getApplication(Long noticeId, Long memberId) {
+        return applicationRepository.findByNoticeNoticeIdAndMemberMemberId(noticeId, memberId);
     }
 
 }
